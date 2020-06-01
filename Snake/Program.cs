f5266af855cc566ba1894cbf3024390af7089df8
AndriyZhalyuk <andr03409@gmail.com>
2020-06-02 01:11:02 +0300

    finish snake

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -9,43 +9,20 @@ namespace Snake
         static void Main(string[] args)
         {
 
-            VerticalLine v1 = new VerticalLine(0, 10, 5, '%');
-            Draw(v1);
-
+            
             Console.SetWindowSize(80, 25);
             // Встановлення розміру консоль
             Console.SetBufferSize(80, 25);
-            
 
-            //Намалювання рамки             
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-            HorizontalLine drownLine = new HorizontalLine(0, 78, 24, '+');
-            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
-            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
-            upLine.Draw();
-            drownLine.Draw();
-            leftLine.Draw();
-            rightLine.Draw();
 
-           
+            //Намалювання рамки    
+            Walls walls = new Walls(80, 25);
+            walls.Draw();     
 
             //Накид точок
             Point p = new Point(4, 5, '*');
-            Figure fSnake = new Snake(p, 4, Direction.RIGHT);
-            Draw(fSnake);
-            Snake snake = (Snake) fSnake;
-
-            HorizontalLine h1 = new HorizontalLine(0, 5, 6, '&');
-
-            List<Figure> figures = new List<Figure>();
-            figures.Add(fSnake);
-            figures.Add(v1);
-            figures.Add(h1);
-
-            foreach(var f in figures)
-            {
-                f.Draw();
-            }
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();                                   
 
             FoodCreator foodCreator = new FoodCreator(80, 25, '$');
             Point food = foodCreator.CreateFood();
@@ -55,30 +32,34 @@ namespace Snake
             
             while (true)
             {
-                if (snake.Eat(food))
-                {
-                    food = foodCreator.CreateFood();
-                    food.Draw();
-                }
-                else
-                {
+                
+                    if (walls.IsHit(snake) || snake.IsHitTail())
+                    {                        
+                        break;
+                    }
+                    if (snake.Eat(food))
+                    {
+                        food = foodCreator.CreateFood();
+                        food.Draw();
+                    }
+                    else
+                    {
+                        snake.Move();
+                    }
+                    Thread.Sleep(100);
+                    if (Console.KeyAvailable)
+                    {
+                        ConsoleKeyInfo key = Console.ReadKey(true);
+                        snake.HandleKey(key.Key);
+
+                    }
                     snake.Move();
-                }
-                Thread.Sleep(150);
-                if (Console.KeyAvailable)
-                {
-                    ConsoleKeyInfo key = Console.ReadKey(true);
-                    snake.HandleKey(key.Key);
-                   
-                }               
-                snake.Move();
-            }
+            }               
+                
+            
             
         }
-        static void Draw(Figure figure)
-        {
-            figure.Draw();
-        }
+       
 
 
     }
